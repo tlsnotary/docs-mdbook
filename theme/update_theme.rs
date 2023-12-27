21c9ee3d7aff566cda855503ef9e67392cb56b91
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2023-12-27 23:21:14 +0100

    Updated theme to v0.4.36 (#56)
    + add script to update the theme automatically

---
+++ theme/update_theme.rs
@@ -0,0 +1,44 @@
+#!/usr/bin/env cargo +nightly -Zscript
+
+// This script fetches the 'index.hbs' file from the mdBook repository and filters the
+// content to only present the 'ayu' theme.
+
+//! ```cargo
+//! [dependencies]
+//! reqwest = { version = "0.11", features = ["blocking"] }
+//! ```
+
+use reqwest::blocking;
+use std::fs::File;
+use std::io::{self, Write};
+
+const MDBOOK_VERSION: &str = "0.4.36";
+
+fn main() -> io::Result<()> {
+    let url = format!(
+        "https://raw.githubusercontent.com/rust-lang/mdBook/v{MDBOOK_VERSION}/src/theme/index.hbs"
+    );
+
+    let response = blocking::get(&url)
+        .expect("Failed fetch template file")
+        .text()
+        .expect("Failed fetch template file");
+
+    // filter unwanted themes: only keep ayu
+    let filtered_content = response
+        .lines()
+        .filter(|line| {
+            if line.contains("role=\"menuitem\"") {
+                line.contains("id=\"ayu\"")
+            } else {
+                true
+            }
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    let mut file = File::create("index.hbs")?;
+    file.write_all(filtered_content.as_bytes())?;
+
+    Ok(())
+}
