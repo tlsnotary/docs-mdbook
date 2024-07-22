9c69b80136e487a5d8ab0919ffdb60a3935fca96
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-07-22 18:01:09 +0200

    feat: add Matomo analytics
    #91

--- theme/update_theme.rs
+++ theme/update_theme.rs
@@ -36,8 +36,25 @@ fn main() -> io::Result<()> {
         .collect::<Vec<_>>()
         .join("\n");
 
+    // Add Matomo tracking script before </body>
+    let tracking_script = r#"      <script>
+            var _paq = window._paq = window._paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+                var u="https://psedev.matomo.cloud/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '16']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src='//cdn.matomo.cloud/psedev.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+        </script>
+    </body>"#;
+
+    let final_content = filtered_content.replace("</body>", tracking_script);
+
     let mut file = File::create("index.hbs")?;
-    file.write_all(filtered_content.as_bytes())?;
+    file.write_all(final_content.as_bytes())?;
 
     Ok(())
 }
